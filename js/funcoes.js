bed6e1feb675471458e5d83d76252254524b18cc
DehMoreira <d-moreira@hotmail.com>
2021-08-29 22:03:59 -0300

    minhas primeiras modificaçoes

---
+++ js/funcoes.js
@@ -0,0 +1,8 @@
+function verTudo(elemento){
+   // elemento.innerHTML = "veja mais...";
+    console.log(document.getElementBytagName("h3"))
+}
+
+function voltar(elemento){
+   // elemento.innerHTML = document.getElementBytagName("h3"); 
+}
